80a55760157a0fca988a1eff30390af625e801d7
github_DASorescu <srsdln97@gmail.com>
2022-08-30 17:45:50 +0200

    snack-2-finished

---
+++ snack2.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = intval($_GET['age']);
+
+$needle1 = '@';
+$needle2 = '.';
+
+if((mb_strlen($name) > 3) && (is_numeric($age)) && (strpos($mail ,$needle1))&& (strpos($mail , $needle2))){
+    echo "ACCESS GRANTED";
+}else{
+    echo "ACCESS DENIED";
+};
+
+
+// var_dump($name);
+// echo mb_strlen($name);
+// var_dump($mail);
+// var_dump($age);
+?>
