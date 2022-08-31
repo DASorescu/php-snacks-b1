9e2ec9fc6c6cd80a3be969e88af995a22199c04c
github_DASorescu <srsdln97@gmail.com>
2022-08-31 10:13:05 +0200

    snack2-refactoring

--- snack2.php
+++ snack2.php
@@ -3,20 +3,58 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// recupero dati
+$name = $_GET['name'] ?? '';
+$mail = $_GET['mail'] ?? '';
+$age = $_GET['age'] ?? '';
 
-$name = $_GET['name'];
-$mail = $_GET['mail'];
-$age = intval($_GET['age']);
 
-$needle1 = '@';
-$needle2 = '.';
+// validazione
+$is_age_valid = is_numeric($age);
+$is_mail_valid = strpos($mail , '@') && strpos($mail , '.');
+$is_name_valid = mb_strlen(trim($name));
 
-if((mb_strlen($name) > 3) && (is_numeric($age)) && (strpos($mail ,$needle1))&& (strpos($mail , $needle2))){
-    echo "ACCESS GRANTED";
-}else{
-    echo "ACCESS DENIED";
-};
+// if(!$is_age_valid || !$is_mail_valid || !$is_name_valid){
+//     echo "ACCESS DENIED";
+    
+// }else{
+//     echo "ACCESS GRANTED";
+// };
 
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .access{
+            height:100vh;
+            width: 100vw;
+            font-size: 150px;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            color:#fff
+        }
+        .granted{
+            background-color:green;
+            
+        }
+        .denied{
+            background-color:#000
+        }
+    </style>
+</head>
+<body>
+    <?php if(!$is_age_valid || !$is_mail_valid || !$is_name_valid) :?>
+        <div class="access denied">ACCESS DENIED</div>
+    <?php else : ?> 
+        <div class="access granted">ACCESS GRANTED</div>
+    <?php endif; ?> 
+</body>
+</html>
